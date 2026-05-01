64542f817c20f79f9536e631d51b22cf381a7e93
PGBmax <levinounet@gmail.com>
2026-05-01 21:58:19 +0200

    Updated Up Tier List (May Up)

--- Ups/script.js
+++ Ups/script.js
@@ -195,7 +195,7 @@ function addDragEvents(img) {
 }
 
 function loadInitialImages() {
-  const total = 51;
+  const total = 57;
   for (let i = 1; i <= total; i++) {
     const img = document.createElement("img");
 	if (i < 10)
